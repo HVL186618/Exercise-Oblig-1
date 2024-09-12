04bb3d754a62906f93fa0938f7c6fc007b045090
HVL186618 <186618@stud.hvl.no>
2024-09-12 14:21:56 +0200

    Laget ny mappe
    ny mappe for uferdig kode.

--- Haskell/Uferdig kode/Not working/Q2c.hs
+++ Haskell/Uferdig kode/Not working/Q2c.hs
@@ -4,9 +4,7 @@ import System.IO (hFlush, stdout)
 
 main = do
     input <- getLine  
-    let a = read input :: Int
     input <- getLine  
-    let b = read input :: Int
     let aBool = a /= 0
     let bBool = b /= 0
     let aStarB = not aBool && not bBool
